b936c15d3465d04edf7e1fa824de092393295b55
dnevidal <doranevidal@gmail.com>
2021-02-26 14:47:41 +0100

    Updated question
    promijenjen konstruktor - prima question text i answer type + dodatno

--- lib/Question.dart
+++ lib/Question.dart
@@ -5,7 +5,10 @@ enum TYPE { Text, RadioButton, Checkbox }
 class Question {
   String questionText;
   TYPE answerType;
-  String answerText;
+  String answerTextT;
+  List<String> answerTextR;
+  List<String> answerTextC;
 
-  Question();
+  Question(this.questionText, this.answerType,
+      {this.answerTextT, this.answerTextR, this.answerTextC});
 }
